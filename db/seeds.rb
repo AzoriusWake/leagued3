fe575128ff96fb85c40107911a88fe4550ed23f6
Henry Tam <IamHenry@Henrys-MacBook-Air.local>
2015-01-10 15:14:34 -0800

    seeding all ready to collect data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 require 'pp'
 require 'httparty'
 
-def get_match
-  match_id = 1688394832
+def get_match(match_id)
   url = "https://na.api.pvp.net/api/lol/na/v2.2/match/#{match_id}?api_key=4f143121-4b49-4dcd-a1b3-ef02c2dc51e7"
   response = HTTParty.get(url)
+  # pp response.code
+  return if response.code != 200
 
   current_match = Match.create(
     matchId:        response["matchId"],
@@ -13,10 +14,8 @@ def get_match
     matchDuration:  response["matchDuration"],
     season:         response["season"]
     )
-
   response["participants"].each do |participant|
-    p participant["highestAchievedSeasonTier"]
-    Player.create(
+    current_match.players.create(
       match_id:                   current_match.id,
       highestAchievedSeasonTier:  participant["highestAchievedSeasonTier"],
       role:                       participant["timeline"]["role"],
@@ -33,4 +32,22 @@ def get_match
   end
 end
 
-get_match
+def collect_data(match_id)
+  50.times do
+    10.times do
+      get_match(match_id)
+      match_id += 1
+    end
+    puts "---> Batch of 10 done <---"
+    puts "---> Last ID: #{match_id} <---"
+    sleep(10) #10 API calls per 10s
+  end
+end
+
+match_id = 1688395000 #need to update every time you seed
+10.times do
+  collect_data(match_id)
+  puts "-------> Batch of 500 done <-------"
+  puts "-------> Last ID: #{match_id} <-------"
+  sleep(600) #500 API calls per 10m
+end
